b52f0340af43d9970c67ca9cbda418958d224119
James Hoang <hoanghiep20179706@gmail.com>
2023-08-01 14:17:41 +0700

    [Reactive MongoDB] Complete config reactive repository

--- src/main/java/james/reactive/web/config/DataConfig.java
+++ src/main/java/james/reactive/web/config/DataConfig.java
@@ -1,13 +1,12 @@
 package james.reactive.web.config;
 
 import de.flapdoodle.embed.mongo.spring.autoconfigure.EmbeddedMongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveRepositoriesAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 @Profile("local")
 @Configuration
-@Import({EmbeddedMongoAutoConfiguration.class, MongoReactiveRepositoriesAutoConfiguration.class})
+@Import({EmbeddedMongoAutoConfiguration.class})
 public class DataConfig {
 }
