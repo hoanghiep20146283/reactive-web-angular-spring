b4a995e197beb6e13038e2f4ccedbab26c6c3df5
James Hoang <hoanghiep20179706@gmail.com>
2023-08-02 13:57:22 +0700

    Add delete API

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,16 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      RouterTestingModule,
+      ReactiveFormsModule,
+      HttpClientModule
+    ],
     declarations: [AppComponent]
   }));
 
