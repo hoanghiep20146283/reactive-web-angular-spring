7208b0c3e20eaebb2d379a0555ce2a1e7175be97
James Hoang <hoanghiep20179706@gmail.com>
2023-08-10 13:46:31 +0700

    Dynamic load embedded MongoDB using reflection

--- src/main/java/james/reactive/web/service/ReservationServiceImpl.java
+++ src/main/java/james/reactive/web/service/ReservationServiceImpl.java
@@ -1,11 +1,11 @@
 package james.reactive.web.service;
 
-import de.flapdoodle.embed.mongo.spring.autoconfigure.MongodWrapper;
 import james.reactive.web.model.Reservation;
 import james.reactive.web.repository.ReactiveReservationRepository;
 import javax.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ReflectionUtils;
@@ -18,7 +18,7 @@ import reactor.core.publisher.Mono;
 public class ReservationServiceImpl implements ReservationService {
 
   private final ReactiveReservationRepository reactiveReservationRepository;
-  private final MongodWrapper mongodWrapper;
+  private final ApplicationContext applicationContext;
 
   @Override
   public Mono<Reservation> getReservation(String id) {
@@ -28,21 +28,21 @@ public class ReservationServiceImpl implements ReservationService {
   @Override
   public Mono<Reservation> createReservation(Mono<Reservation> reservationMono) {
     return reservationMono.flatMap(
-        reactiveReservationRepository::save);
+      reactiveReservationRepository::save);
   }
 
   @Override
   public Mono<Reservation> updateReservation(String id, Mono<Reservation> reservationMono) {
     return reservationMono.flatMap(
-            reservation -> reactiveReservationRepository.findById(id).flatMap(existing -> {
-              existing.setPrice(reservation.getPrice());
-              existing.setCheckIn(reservation.getCheckIn());
-              existing.setCheckOut(reservation.getCheckOut());
-              existing.setRoomNumber(reservation.getRoomNumber());
-              return Mono.just(existing);
-            }))
-        .flatMap(reactiveReservationRepository::save)
-        .switchIfEmpty(Mono.error(NotFoundException::new));
+        reservation -> reactiveReservationRepository.findById(id).flatMap(existing -> {
+          existing.setPrice(reservation.getPrice());
+          existing.setCheckIn(reservation.getCheckIn());
+          existing.setCheckOut(reservation.getCheckOut());
+          existing.setRoomNumber(reservation.getRoomNumber());
+          return Mono.just(existing);
+        }))
+      .flatMap(reactiveReservationRepository::save)
+      .switchIfEmpty(Mono.error(NotFoundException::new));
   }
 
   @Override
@@ -58,7 +58,17 @@ public class ReservationServiceImpl implements ReservationService {
   @PreDestroy
   public void clearMongoEmbedded() {
     log.info("Closing");
-    ReflectionUtils.invokeMethod(ReflectionUtils.findMethod(mongodWrapper.getClass(), "stop"),
-        mongodWrapper);
+    try {
+      Class<?> mongoDWrapperClass = Class.forName(
+        "de.flapdoodle.embed.mongo.spring.autoconfigure.MongodWrapper");
+      ReflectionUtils.invokeMethod(ReflectionUtils.findMethod(mongoDWrapperClass, "stop"),
+        applicationContext.getBean(mongoDWrapperClass));
+    } catch (ClassNotFoundException e) {
+      log.info("No Embedded MongoDB in classpath, skip!");
+      Exception exception = new RuntimeException("Test");
+      log.error("Unknown error: {}", exception.getMessage(), exception);
+    } catch (Exception exception) {
+      log.error("Unknown error: {}", exception.getMessage(), exception);
+    }
   }
 }
