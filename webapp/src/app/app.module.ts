d58139d9c92c2cc9c73c07e6a72a023e58611d47
Hoang Hiep <hoanghiep20179706@gmail.com>
2023-11-07 10:21:59 +0700

    Fix import component standalone

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { AppService } from './app.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [BrowserModule, MatCardModule, MatInputModule],
-  providers: [],
+  imports: [BrowserAnimationsModule, BrowserModule, MatCardModule, MatInputModule],
+  providers: [ AppService, MatSnackBar],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
