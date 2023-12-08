ddffd0187f3156277f0b9cfc7262307ae544fea1
Hiệp Hoàng <hoanghiep20179706@gmail.com>
2023-12-08 14:20:06 +0700

    Working with event in custom directives (hovering via hostListener)

--- webapp/src/app/tutorial/directive-examples/custom.directive.ts
+++ webapp/src/app/tutorial/directive-examples/custom.directive.ts
@@ -1,8 +1,16 @@
-import { Directive, HostBinding, Input } from "@angular/core";
+import { Directive, HostBinding, HostListener, Input } from "@angular/core";
 
-@Directive({selector: '[appCustomDirectiveExamples]'})
+@Directive({ selector: '[appCustomDirectiveExamples]' })
 export class CustomDirective {
     @HostBinding('class') customName = 'undefined';
+    @HostBinding('class.is-hovering') isHovering = false;
+    @HostListener('mouseenter') onMouseEnter() {
+        this.isHovering = true;
+    }
+
+    @HostListener('mouseleave') onMouseLeave() {
+        this.isHovering = false;
+    }
 
     @Input() set appCustomDirectiveExamples(value) {
         this.customName = value + "-CustomDirectiveValue";
