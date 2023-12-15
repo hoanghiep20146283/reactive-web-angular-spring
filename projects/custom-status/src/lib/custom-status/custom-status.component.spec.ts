ea7392ea83123b2666bb6b390878ddfcf00acd03
Hoang Hiep <hoanghiep20179706@gmail.com>
2023-12-15 11:45:18 +0700

    Add snapshot testing

--- projects/custom-status/src/lib/custom-status/custom-status.component.spec.ts
+++ projects/custom-status/src/lib/custom-status/custom-status.component.spec.ts
@@ -6,11 +6,46 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 TestBed.initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
+
+@Component({
+  template: `<lib-custom-status [customStatus]="status"></lib-custom-status>`,
+})
+class TestCustomStatusComponent {
+  status!: string;
+}
+
+describe('TestCustomStatusComponent', () => {
+  let testCustomStatusComponent: TestCustomStatusComponent;
+  let fixture: ComponentFixture<TestCustomStatusComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CustomStatusComponent, TestCustomStatusComponent],
+    }).compileComponents();
+    fixture = TestBed.createComponent(TestCustomStatusComponent);
+    testCustomStatusComponent = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(testCustomStatusComponent).toBeTruthy();
+  });
+
+  it('should set color to green if input status is safe', () => {
+    testCustomStatusComponent.status = 'safe';
+    fixture.detectChanges();
+    const pElement: HTMLElement = fixture.debugElement.query(
+      By.css('p')
+    ).nativeElement;
+    expect(pElement.classList).toContain('green');
+  });
+});
 
 describe('CustomStatusComponent', () => {
   let component: CustomStatusComponent;
@@ -27,9 +62,5 @@ describe('CustomStatusComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should not create', () => {
-    expect(component).toBeFalsy();
   });
 });
